c4d02b91fda6177509d7761913804516e3005765
ANONYMES01 <124345325+ANONYMES01@users.noreply.github.com>
2023-02-03 09:20:49 +0100

    pseudo hasard.py

---
+++ pseudo hasard.py
@@ -0,0 +1,7 @@
+import random
+
+print("Je te génère un pseudo random.")
+
+liste_pseudo = ["4ver", "ZBest" "FantaSious", "MoSTWanTeD", "ReLaoDeD", "2Late", "Adren4lYnne", "Bullet", "KardiaK", "NeoN", "TheCrew"]
+
+print(random.choices(liste_pseudo))
